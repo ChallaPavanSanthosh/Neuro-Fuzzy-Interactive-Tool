a0345bee8608c73e63c30c52e4159eb508edaed8
ChallaPavanSanthosh <chvspsanthosh@gmail.com>
2024-05-08 23:40:09 +0530

    update

---
+++ question.js
@@ -0,0 +1,50 @@
+let questions = [
+  {
+    numb: 1,
+    question: "How many types are there in classification of genetic algorithm?",
+    answer: "6",
+    options: [
+      "3",
+      "5",
+      "6",
+      "4",
+    ],
+  },
+  {
+    numb: 2,
+    question: "Which of the following is not a solution representation in a genetic algorithm",
+    answer: "Decentralized",
+    options: ["Hierarchical", "Network", "Distributed", "Decentralized"],
+  },
+  {
+    numb: 3,
+    question:
+      "The FMGA is a ~~__________~~ to solving optimization problems",
+    answer: "All the above",
+    options: [
+      "Binary",
+      "Stochastic",
+      "Population Based approach",
+      "All the above",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "Which approach of PGA involves only one population with each member interacting with a limited set of others?",
+    answer: "Fine-grained GA",
+    options: ["Standard parallel approach", "Decomposition approach", "Fine-grained GA", "Coarse-grained GA"],
+  },
+  {
+    numb: 5,
+    question: "Which of the following is not a mutation operator in genetic algorithm",
+    answer: "Difference",
+    options: ["Random resetting", "Scramble", "Difference", "Inversion"],
+  },
+  {
+    numb: 6,
+    question: "Please, Rate this Portal!",
+    answer: "None",
+    options: ["Good", "Need some improvement", "Not good", "None"],
+  },
+];
